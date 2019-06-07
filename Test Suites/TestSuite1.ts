7b96b9f3555a7e207fbd20a4d160fd66f803572f
Maximiliano Adrian Vidarte <mvidarte@practia.global>
2019-06-07 12:25:39 -0300

    se agrego un test suite

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>96dcad05-f032-49e9-959a-b585985d5d2f</testSuiteGuid>
+   <testCaseLink>
+      <guid>5f209519-00b5-463d-a294-bbd219488905</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab6ffd62-5652-4af5-89db-546367db8046</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
